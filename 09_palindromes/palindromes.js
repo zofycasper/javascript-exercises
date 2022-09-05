2106e9a86c27cd64527ec680afd776160d6a3345
zofycasper <zofycasper@gmail.com>
2022-09-06 01:16:41 +0800

    08 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let words = Array.from(string.toLowerCase());
+    let newString = [];
 
+    for (let i = 0; i < words.length; i++) {
+        if (
+            words[i] != " " &&
+            words[i] != "!" &&
+            words[i] != "," &&
+            words[i] != "."
+        ) {
+            newString.push(words[i]);
+        }
+    }
+
+    let length = newString.length;
+    let mid = Math.floor(length / 2);
+    let count = 0;
+    for (let i = 0; i < mid; i++) {
+        if (newString[i] == newString[length - 1 - i]) {
+            count++;
+        }
+    }
+    if (count == mid) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
