6587c1de436c4614c406cccbc14551db0084fafd
zofycasper <zofycasper@gmail.com>
2022-09-08 15:01:51 +0800

    all done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const oldest = people.reduce((a, b) => {
+        if (getAge(a) > getAge(b)) {
+            return a;
+        } else return b;
+    });
+    return oldest;
 };
+
+function getAge(a) {
+    if (!a.yearOfDeath) {
+        a.yearOfDeath = new Date().getFullYear();
+    }
+    let age = a.yearOfDeath - a.yearOfBirth;
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
